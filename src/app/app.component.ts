2143ab2505e4d174495c136a44788d9a13b16c0b
Adam <adam.tremblett7@gmail.com>
2020-04-06 11:04:14 -0400

    Getting Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,15 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     this.http
       .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+        'https://angular-http-95a20.firebaseio.com/posts.json',
         postData
       )
       .subscribe(responseData => {
@@ -27,9 +29,16 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get('https://angular-http-95a20.firebaseio.com/posts.json').subscribe(posts => {
+      console.log(posts);
+    });
   }
 }
